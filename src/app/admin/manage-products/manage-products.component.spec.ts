b207eda9faca736280f9c2aa8a1126c572356ccc
ramjonathan <jonathan_ramirez1@epam.com>
2026-05-28 01:09:33 +0000

    fix: unit test

--- src/app/admin/manage-products/manage-products.component.spec.ts
+++ src/app/admin/manage-products/manage-products.component.spec.ts
@@ -1,14 +1,37 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ManageProductsComponent } from './manage-products.component';
+import { CONFIG_TOKEN } from 'src/app/core/injection-tokens/config.token';
 
-describe('ProductsComponent', () => {
+describe('ManageProductsComponent', () => {
   let component: ManageProductsComponent;
   let fixture: ComponentFixture<ManageProductsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ManageProductsComponent],
+      provides: [
+        {
+          provide: CONFIG_TOKEN,
+          useValue: {
+            apiEndpoints: {
+              product: '',
+              order: '',
+              import: '',
+              bff: '',
+              cart: '',
+            },
+            apiEndpointsEnabled: {
+              product: false,
+              order: false,
+              import: false,
+              bff: false,
+              cart: false,
+            },
+            production: false,
+          },
+        },
+      ],
     }).compileComponents();
   });
 
