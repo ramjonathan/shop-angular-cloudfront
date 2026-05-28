55c07c71c279a62dca0620f632a06ce691578059
ramjonathan <jonathan_ramirez1@epam.com>
2026-05-28 00:11:12 +0000

    fix: unit test

--- src/app/cart/cart-shipping-form/cart-shipping-form.component.spec.ts
+++ src/app/cart/cart-shipping-form/cart-shipping-form.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormGroup, FormControl } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { CartShippingFormComponent } from './cart-shipping-form.component';
 
@@ -9,7 +10,7 @@ describe('CartShippingFormComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CartShippingFormComponent],
+      imports: [CartShippingFormComponent, NoopAnimationsModule],
     }).compileComponents();
   });
 
