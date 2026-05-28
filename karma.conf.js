4705eb3a53a518eee7a16cb6ce8e4f0c6d876b19
ramjonathan <jonathan_ramirez1@epam.com>
2026-05-27 20:09:33 -0600

    Refactor Jasmine configuration and coverage reporter

--- karma.conf.js
+++ karma.conf.js
@@ -13,22 +13,22 @@ module.exports = function (config) {
       require("@angular-devkit/build-angular/plugins/karma"),
     ],
     client: {
-      jasmine: {
-        // you can add configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
-      },
-      clearContext: false, // leave Jasmine Spec Runner output visible in browser
+      jasmine: {},
+      clearContext: false,
     },
     jasmineHtmlReporter: {
-      suppressAll: true, // removes the duplicated traces
-    },
+      suppressAll: true,
+    },    
     coverageReporter: {
-      dir: require("path").join(__dirname, "./coverage/app"),
+      dir: require("path").join(__dirname, "./coverage"),
       subdir: ".",
-      reporters: [{ type: "html" }, { type: "text-summary" }],
+      reporters: [
+        { type: "html" }, 
+        { type: "text-summary" },
+        { type: "lcovonly" }
+      ],
     },
+
     reporters: ["spec"],
     port: 9876,
     colors: true,
